2253586567b1675a7b6c2bbb684fecc9ebf6e5fe
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-01-13 19:08:45 +0900

    Add Utility/Msg.java
    メッセージ関連の関数を作成

---
+++ src/main/java/net/jp/minecraft/plugins/Utility/Msg.java
@@ -0,0 +1,139 @@
+package net.jp.minecraft.plugins.Utility;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+
+/**
+ * TeisyokuPlugin2
+ *
+ * @auther syokkendesuyo
+ */
+public final class Msg{
+
+    //基本のPrefix
+    public final static String prefix = "[Teisyoku] " + ChatColor.DARK_GRAY + ">" + ChatColor.GRAY + ">" + " ";
+    public final static String broadcast = "[Teisyoku|Broadcast] " + ChatColor.DARK_GRAY + ">" + ChatColor.GRAY + ">" + " ";
+
+    /**
+     *Successのメッセージをプレイヤーに送信
+     * @param p プレイヤー
+     * @param msg メッセージ
+     * @param broadcast
+     */
+    public static void success(final Player p , final String msg , boolean broadcast){
+        if(broadcast == true){
+            Bukkit.broadcastMessage(ChatColor.GREEN + prefix + ChatColor.RESET + msg);
+        }else{
+            p.sendMessage(ChatColor.GREEN + prefix + ChatColor.RESET + msg);
+        }
+    }
+
+    /**
+     *Successのメッセージをプレイヤーに送信
+     * @param p プレイヤー
+     * @param msg メッセージ
+     */
+    public static void success(final Player p , final String msg){
+        success(p,msg,false);
+    }
+
+    /**
+     *Warningのメッセージをプレイヤーに送信
+     * @param p プレイヤー
+     * @param msg メッセージ
+     * @param broadcast
+     */
+    public static void warning(final Player p , final String msg , boolean broadcast){
+        if(broadcast == true){
+            Bukkit.broadcastMessage(ChatColor.RED + prefix + ChatColor.RESET + msg);
+        }else{
+            p.sendMessage(ChatColor.RED + prefix + ChatColor.RESET + msg);
+        }
+    }
+
+    /**
+     *Warningのメッセージをプレイヤーに送信
+     * @param p プレイヤー
+     * @param msg メッセージ
+     */
+    public static void warning(final Player p , final String msg){
+        warning(p,msg,false);
+    }
+
+    /**
+     *Infoのメッセージをプレイヤーに送信
+     * @param p プレイヤー
+     * @param msg メッセージ
+     * @param broadcast
+     */
+    public static void info(final Player p , final String msg , boolean broadcast){
+        if(broadcast == true){
+            Bukkit.broadcastMessage(ChatColor.YELLOW + prefix + ChatColor.RESET + msg);
+        }else{
+            p.sendMessage(ChatColor.YELLOW + prefix + ChatColor.RESET + msg);
+        }
+    }
+
+    /**
+     *Infoのメッセージをプレイヤーに送信
+     * @param p プレイヤー
+     * @param msg メッセージ
+     */
+    public static void info(final Player p , final String msg){
+        info(p,msg,false);
+    }
+
+    /**
+     *Adminにのみブロードキャストメッセージを送信
+     * @param msg メッセージ
+     * @param sound
+     */
+    public static void adminBroadcast(String msg , boolean sound){
+        for(Player admin : Bukkit.getOnlinePlayers()){
+            if(admin.hasPermission("teisyoku.admin")){
+                admin.sendMessage(ChatColor.LIGHT_PURPLE + broadcast + ChatColor.RESET + msg);
+                if(sound == true){
+                    admin.playSound(admin.getLocation() , Sound.NOTE_PLING , 3.0F,1.5F);
+                }
+            }
+        }
+    }
+
+    /**
+     *Adminにのみブロードキャストメッセージを送信
+     * @param msg メッセージ
+     */
+    public static void opBroadcast(String msg) {
+        adminBroadcast(msg , true);
+    }
+
+    /**
+     *パーミッションエラーを返す時のスタイル
+     * @param p プレイヤー
+     * @param perm パーミッション
+     */
+    public static void getNoPermissionMessage(Player p , String perm){
+        Msg.warning(p , "パーミッションがありません" + getPermissionNode(perm));
+    }
+
+    /**
+     *パーミッションを表示する時のスタイル
+     * @param p プレイヤー
+     * @param perm パーミッション
+     */
+    public static void getCheckPermissionMessage(Player p , String perm){
+        Msg.info(p , "パーミッション：" + getPermissionNode(perm));
+    }
+
+    /**
+     *パーミッションノードを灰色で表示
+     * @param permission パーミッション
+     * @return パーミッション専用スタイル
+     */
+    public static String getPermissionNode(String permission){
+        String string = ChatColor.GRAY + permission + ChatColor.RESET;
+        return string;
+    }
+}

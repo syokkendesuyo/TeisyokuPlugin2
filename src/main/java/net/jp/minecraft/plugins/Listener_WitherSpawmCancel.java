b770c571b4e79a5af252024013005df5119363df
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-02-29 19:12:57 +0900

    Delete debug message

--- src/main/java/net/jp/minecraft/plugins/Listener_WitherSpawmCancel.java
+++ src/main/java/net/jp/minecraft/plugins/Listener_WitherSpawmCancel.java
@@ -1,7 +1,5 @@
 package net.jp.minecraft.plugins;
 
-import net.jp.minecraft.plugins.Utility.Msg;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,7 +18,6 @@ public class Listener_WitherSpawmCancel implements Listener {
         if (event.getEntityType() == EntityType.WITHER){
             List<String> worlds = TeisyokuPlugin2.getInstance().TeisyokuConfig.getStringList("arrow_summon_wither");
             for(String s : worlds){
-                Bukkit.broadcastMessage("Data: " + s + " " + event.getLocation().getWorld().getName().toString());
                 if (!(event.getLocation().getWorld().getName().toString().equals(s))){
                     event.setCancelled(true);
                 }

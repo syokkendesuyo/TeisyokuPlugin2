38dab23cc49fdbb044ae9563a3a7adc0bc950ae7
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-02-29 19:04:44 +0900

    Fix Wither Spawn Event
    Teisyoku.ymlに「arrow_summon_wither」が生成されるようになり、ウイザーをスポーンさせることのできるワールドを限定できるように修正(複数登録可能)

--- src/main/java/net/jp/minecraft/plugins/Listener_WitherSpawmCancel.java
+++ src/main/java/net/jp/minecraft/plugins/Listener_WitherSpawmCancel.java
@@ -1,10 +1,13 @@
 package net.jp.minecraft.plugins;
 
+import net.jp.minecraft.plugins.Utility.Msg;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+
+import java.util.List;
 
 /**
  * TeisyokuPlugin2
@@ -15,8 +18,12 @@ public class Listener_WitherSpawmCancel implements Listener {
     @EventHandler
     public void onCreatureSpawn(EntitySpawnEvent event){
         if (event.getEntityType() == EntityType.WITHER){
-            if(!(event.getLocation().getWorld().getName().equals("world_the_end"))){
-                event.setCancelled(true);
+            List<String> worlds = TeisyokuPlugin2.getInstance().TeisyokuConfig.getStringList("arrow_summon_wither");
+            for(String s : worlds){
+                Bukkit.broadcastMessage("Data: " + s + " " + event.getLocation().getWorld().getName().toString());
+                if (!(event.getLocation().getWorld().getName().toString().equals(s))){
+                    event.setCancelled(true);
+                }
             }
         }
     }

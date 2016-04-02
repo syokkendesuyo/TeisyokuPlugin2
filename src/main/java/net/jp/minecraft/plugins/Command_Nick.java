ddc9163c655554031844005eb0a14cdc194e2a62
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-04-03 02:50:08 +0900

    Fix nick

--- src/main/java/net/jp/minecraft/plugins/Command_Nick.java
+++ src/main/java/net/jp/minecraft/plugins/Command_Nick.java
@@ -1,5 +1,6 @@
 package net.jp.minecraft.plugins;
 
+import net.jp.minecraft.plugins.Utility.Msg;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
@@ -7,6 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import sun.applet.resources.MsgAppletViewer_de;
 
 /**
  * TeisyokuPlugin2
@@ -46,48 +48,52 @@ public class Command_Nick implements CommandExecutor {
 
             if(args[1].equalsIgnoreCase("set")){
                 if(! (args.length == 4)){
-                    sender.sendMessage(Messages.getDenyPrefix() + "引数が多すぎるかまたは少なすぎます");
+                    Msg.warning(sender, "引数が多すぎるかまたは少なすぎます");
                     HelpMessage(sender, cmd);
                     AdminHelpMessage(sender, cmd);
                     return true;
                 }
 
                 //オンラインかどうかの確認
-                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[2]);
-                if(! offlinePlayer.isOnline()){
-                    sender.sendMessage(Messages.getDenyPrefix() + ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんはオンラインでない為操作できません");
+                Player player = Bukkit.getServer().getPlayer(args[2]);
+                if( !(player == null) ){
+                    //正常処理
+                    Player onlinePlayer = player;
+                    setConfig(player, args[3]);
+                    Msg.success(sender, ChatColor.YELLOW + args[2] + ChatColor.RESET + " さんのニックネームを " + ChatColor.AQUA + args[3] + ChatColor.RESET +" に設定しました");
+                    Msg.success(onlinePlayer, ChatColor.YELLOW + sender.getName() + ChatColor.RESET + " さんによってニックネームを " + ChatColor.AQUA + args[3] + ChatColor.RESET + " に設定されました");
                     return true;
                 }
-
-                //正常処理
-                Player onlinePlayer = (Player) offlinePlayer;
-                setConfig((Player) offlinePlayer, args[3]);
-                sender.sendMessage(Messages.getSuccessPrefix() + ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんのニックネームを " + ChatColor.AQUA + args[3] + ChatColor.RESET +" に設定しました");
-                onlinePlayer.sendMessage(Messages.getSuccessPrefix() + ChatColor.YELLOW + sender.getName() + ChatColor.RESET + "さんによってニックネームを " + ChatColor.AQUA + args[3] + ChatColor.RESET + " に設定されました");
-                return true;
+                else{
+                    //プレイヤーが居ない場合の処理
+                    Msg.warning(sender, ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんはオンラインでない為操作できません");
+                    return true;
+                }
             }
 
             if(args[1].equalsIgnoreCase("remove")){
                 if(! (args.length == 3)){
-                    sender.sendMessage(Messages.getDenyPrefix() + "引数が多すぎるかまたは少なすぎます");
+                    Msg.success(sender, "引数が多すぎるかまたは少なすぎます");
                     HelpMessage(sender, cmd);
                     AdminHelpMessage(sender, cmd);
                     return true;
                 }
 
                 //オンラインかどうかの確認
-                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[2]);
-                if(! offlinePlayer.isOnline()){
-                    sender.sendMessage(Messages.getDenyPrefix() + ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんはオンラインでない為操作できません");
+                Player player = Bukkit.getServer().getPlayer(args[2]);
+                if( !(player == null) ){
+                    //正常処理
+                    Player onlinePlayer = player;
+                    removeConfig(player);
+                    Msg.success(sender, ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんのニックネームを削除しました");
+                    Msg.success(onlinePlayer, ChatColor.YELLOW + sender.getName() + ChatColor.RESET + "さんによってニックネームを削除されました");
                     return true;
                 }
-
-                //正常処理
-                Player onlinePlayer = (Player) offlinePlayer;
-                removeConfig((Player) offlinePlayer);
-                sender.sendMessage(Messages.getSuccessPrefix() + ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんのニックネームを削除しました");
-                onlinePlayer.sendMessage(Messages.getSuccessPrefix() + ChatColor.YELLOW + sender.getName() + ChatColor.RESET + "さんによってニックネームを削除されました");
-                return true;
+                else{
+                    //プレイヤーが居ない場合の処理
+                    Msg.warning(sender, ChatColor.YELLOW + args[2] + ChatColor.RESET + "さんはオンラインでない為操作できません");
+                    return true;
+                }
             }
             return true;
         }
@@ -103,8 +109,8 @@ public class Command_Nick implements CommandExecutor {
 
         //下記コマンドに侵入するにはsenderがplayerである必要があります
         if(!(sender instanceof Player)){
-            sender.sendMessage(Messages.getDenyPrefix() + "コンソールからコマンドを送信することはできません");
-            sender.sendMessage(Messages.getDenyPrefix() + "ただし /"+ cmd.getName() +" admin は利用可能です");
+            Msg.success(sender, "コンソールからコマンドを送信することはできません");
+            Msg.success(sender, "ただし /"+ cmd.getName() +" admin は利用可能です");
             return true;
         }
 
@@ -115,17 +121,17 @@ public class Command_Nick implements CommandExecutor {
         if(args[0].equalsIgnoreCase("set")){
             if(args.length == 2){
                 if(args[1].length() > 10){
-                    sender.sendMessage(Messages.getDenyPrefix() + "ニックネームは10文字以下に設定してください");
+                    Msg.warning(sender, "ニックネームは10文字以下に設定してください");
                     return true;
                 }
                 else{
                     setConfig(player, args[1]);
-                    sender.sendMessage(Messages.getSuccessPrefix() + "ニックネームを " + ChatColor.AQUA + args[1] + ChatColor.RESET + " に設定しました");
+                    Msg.success(sender, "ニックネームを " + ChatColor.AQUA + args[1] + ChatColor.RESET + " に設定しました");
                     return true;
                 }
             }
             else{
-                sender.sendMessage(Messages.getDenyPrefix() + "引数が多すぎるかまたは少なすぎます");
+                Msg.warning(sender, "引数が多すぎるかまたは少なすぎます");
                 HelpMessage(sender, cmd);
                 return true;
             }
@@ -135,29 +141,29 @@ public class Command_Nick implements CommandExecutor {
         if(args[0].equalsIgnoreCase("remove")){
             if(args.length == 1){
                 removeConfig(player);
-                sender.sendMessage(Messages.getSuccessPrefix() + "ニックネームを削除しました");
+                Msg.success(sender, "ニックネームを削除しました");
                 return true;
             }
             else{
-                sender.sendMessage(Messages.getDenyPrefix() + "引数が多すぎるかまたは少なすぎます");
+                Msg.warning(sender, "引数が多すぎるかまたは少なすぎます");
                 HelpMessage(sender, cmd);
                 return true;
             }
         }
 
-        sender.sendMessage(Messages.getDenyPrefix() + "引数 " + args[0].toString()  + " は存在しません");
+        Msg.warning(sender, "引数 " + args[0].toString()  + " は存在しません");
         return true;
     }
 
     public void HelpMessage(CommandSender sender , Command cmd){
-        sender.sendMessage(Messages.getSuccessPrefix() + cmd.getName() + "コマンドのヘルプ" + ChatColor.GRAY +"(開発協力:azuhataさん)");
-        sender.sendMessage(Messages.getCommandFormat(cmd.getName() + " set <ﾆｯｸﾈｰﾑ>", "自分のﾆｯｸﾈｰﾑをｾｯﾄします"));
-        sender.sendMessage(Messages.getCommandFormat(cmd.getName() + " remove","自分のﾆｯｸﾈｰﾑを削除します"));
+        Msg.success(sender, "コマンドのヘルプ" + ChatColor.GRAY +"(開発協力:azuhataさん)");
+        Msg.commandFormat(sender, cmd.getName() + " set <ﾆｯｸﾈｰﾑ>", "自分のﾆｯｸﾈｰﾑをｾｯﾄします");
+        Msg.commandFormat(sender, cmd.getName() + " remove","自分のﾆｯｸﾈｰﾑを削除します");
     }
 
     public void AdminHelpMessage(CommandSender sender , Command cmd){
-        sender.sendMessage(Messages.getCommandFormat(cmd.getName() + " admin set <ﾌﾟﾚｲﾔｰ名> <ﾆｯｸﾈｰﾑ>","他ﾌﾟﾚｲﾔｰのﾆｯｸﾈｰﾑをｾｯﾄします"));
-        sender.sendMessage(Messages.getCommandFormat(cmd.getName() + " admin remove <ﾌﾟﾚｲﾔｰ名>","他ﾌﾟﾚｲﾔｰのﾆｯｸﾈｰﾑを削除します"));
+        Msg.commandFormat(sender, cmd.getName() + " admin set <ﾌﾟﾚｲﾔｰ名> <ﾆｯｸﾈｰﾑ>","他ﾌﾟﾚｲﾔｰのﾆｯｸﾈｰﾑをｾｯﾄします");
+        Msg.commandFormat(sender, cmd.getName() + " admin remove <ﾌﾟﾚｲﾔｰ名>","他ﾌﾟﾚｲﾔｰのﾆｯｸﾈｰﾑを削除します");
     }
 
     private void setConfig(Player player, String NickName) {

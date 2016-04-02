ba31473374469ef223bd0838ba435a829d246024
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-04-02 15:31:13 +0900

    Fix Gomibako Messages

--- src/main/java/net/jp/minecraft/plugins/Command_Gomibako.java
+++ src/main/java/net/jp/minecraft/plugins/Command_Gomibako.java
@@ -1,5 +1,6 @@
 package net.jp.minecraft.plugins;
 
+import net.jp.minecraft.plugins.Utility.Msg;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,7 +14,7 @@ public class Command_Gomibako implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if(! (sender.hasPermission(Permissions.getGomibakoCommandPermisson()))){
-            sender.sendMessage(Messages.getNoPermissionMessage(Permissions.getGomibakoCommandPermisson()));
+            Msg.noPermissionMessage(sender,Permissions.getGomibakoCommandPermisson());
             return true;
         }
 
@@ -22,11 +23,11 @@ public class Command_Gomibako implements CommandExecutor {
         }
         else if(args.length == 1){
             if(args[0].equalsIgnoreCase("perm")||args[0].equalsIgnoreCase("permission")){
-                sender.sendMessage(Messages.getNormalPrefix() + "パーミッション(通常利用): " + Permissions.getGomibakoPermisson());
-                sender.sendMessage(Messages.getNormalPrefix() + "パーミッション(コマンド): " + Permissions.getGomibakoCommandPermisson());
+                Msg.info(sender, "パーミッション(通常利用): " + Permissions.getGomibakoPermisson());
+                Msg.info(sender, "パーミッション(コマンド): " + Permissions.getGomibakoCommandPermisson());
             }
            else{
-                sender.sendMessage(Messages.getDenyPrefix() + "引数「" + args[0] + "」は存在しません");
+                Msg.warning(sender,"引数「" + args[0] + "」は存在しません");
             }
         }
         return true;

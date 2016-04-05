9866362e7b98ce5b4fc7d7fcac974312b81ec1a2
azuhata0105 <hibiki20010105@i.softbank.jp>
2016-04-05 22:41:17 +0900

    Change MineCart etc...

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_CartHelp.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_CartHelp.java
@@ -19,13 +19,12 @@ public class Command_CartHelp implements CommandExecutor {
             
         }
         Msg.info(sender, ChatColor.AQUA + "---------------[MineCartHelp]---------------" + ChatColor.GRAY + "(開発:azuhata)", false);
-        Msg.info(sender, "Minecart看板 : 看板の1行目に[Minecart] 3行目にShinkansen Express Local SightSeeing のどれかを入力", false);
+        Msg.info(sender, "Minecart看板 : 看板の1行目に[Minecart] 3行目に SightSeeing を入力", false);
         Msg.info(sender, "アクティベーターレール : 最高速度を通常Minecartの最高速度に再設定します", false);
         Msg.info(sender, "Shinkansen対応ブロック : 鉄ブロック 黒曜石", false);
-        Msg.info(sender, "Express対応ブロック : 石 レッドストーンブロック 鉄ブロック ハーフブロック 石レンガ 砂利 黒曜石", false);
-        Msg.info(sender, "Local対応ブロック : レールが設置できるものすべて", false);
+        Msg.info(sender, "Express対応ブロック : 石 レッドストーンブロック ハーフブロック 石レンガ 砂利 ", false);
         Msg.info(sender, "SightSeeing対応ブロック : レールが設置できるものすべて", false);
-        Msg.info(sender, "注意点 : Shinkansenは超高速であるため、カーブや坂の前は必ず減速してください", false);
+        Msg.info(sender, "注意点 : Shinkansen / Express は超高速であるため、カーブや坂の前は必ず減速してください", false);
         Msg.info(sender, ChatColor.AQUA + "--------------------------------------------", false);
         
         

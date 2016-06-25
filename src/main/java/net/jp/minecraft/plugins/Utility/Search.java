f2a6d6bf2dd72a35ca1d88c3beb463d848d3a40d
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-06-26 02:17:37 +0900

    Fix Search method

--- src/main/java/net/jp/minecraft/plugins/Utility/Search.java
+++ src/main/java/net/jp/minecraft/plugins/Utility/Search.java
@@ -10,7 +10,11 @@ package net.jp.minecraft.plugins.Utility;
 public class Search {
 
     /**
+     * String配列に対して検索をするメソッド
+     * 部分一致でtrueを返します
      *
+     * @param list 検索する文字列型の配列
+     * @param keyword 検索する文字列
      * @return boolean
      */
     public static boolean searchKeyword(final String[] list, final String keyword) {

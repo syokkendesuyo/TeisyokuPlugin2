60c07ab6005ee92196dcae2408f9114378af4584
syokkendesuyo <my_cra@mail.minecraft.jp.net>
2016-07-14 01:52:36 +0900

    Update players command

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Players.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Players.java
@@ -1,5 +1,6 @@
 package net.jp.minecraft.plugins.Commands;
 
+import net.jp.minecraft.plugins.API.API_Players;
 import net.jp.minecraft.plugins.GUI.GUI_PlayersList;
 import net.jp.minecraft.plugins.Permissions;
 import net.jp.minecraft.plugins.Utility.Msg;
@@ -29,7 +30,22 @@ public class Command_Players implements CommandExecutor {
 
         Player player = (Player)sender;
 
+        if(args.length == 0){
+            GUI_PlayersList.getPlayersList(player);
+            return true;
+        }
+
+        if(args.length == 1){
+            if(!(args[0].equalsIgnoreCase("total"))){
+                GUI_PlayersList.getPlayersList(player);
+                return true;
+            }
+            Msg.info(sender, "総計 " + API_Players.getTotalPlayers() + " 名のプレイヤーがこのサーバで遊びました");
+            return true;
+        }
+
         GUI_PlayersList.getPlayersList(player);
+
         return true;
     }
 }

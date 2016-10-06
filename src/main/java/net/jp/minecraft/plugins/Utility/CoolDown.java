99f8d7cf9d58da1ebe81f9a7ef83b71bad76578e
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:32:19 +0900

    CTの修正

--- src/main/java/net/jp/minecraft/plugins/Utility/CoolDown.java
+++ src/main/java/net/jp/minecraft/plugins/Utility/CoolDown.java
@@ -13,12 +13,12 @@ import java.util.*;
 public class CoolDown {
 
     //HashMapの初期値を最大ログイン人数にする
-    public static int n = Bukkit.getMaxPlayers();
+    private static int n = Bukkit.getMaxPlayers();
 
     //HashMapを生成、＜プレイヤーのUUID,クールダウン終了の時刻を格納＞
-    public static Map<UUID, Calendar> map = new HashMap<UUID, Calendar>(n * 4 / 3);
+    private static Map<UUID, Calendar> map = new HashMap<UUID, Calendar>(n * 4 / 3);
 
-    public static boolean cooldown(Player player){
+    public static boolean cooldown(Player player) {
         //現在時刻を取得
         Date nowDate = new Date();
 
@@ -29,34 +29,33 @@ public class CoolDown {
         //終了時刻
         Calendar check = get(player);
 
-        if(check.before(now) == true){
+        if (check.before(now)) {
             return true;
-        }
-        else{
+        } else {
             long difference = check.getTime().getTime() - now.getTime().getTime();
-            Msg.warning(player,"コマンドを実行できませんでした： 残り " + difference/1000 + " 秒");
+            Msg.warning(player, "コマンドを実行できませんでした： 残り " + difference / 1000 + " 秒");
             return false;
         }
     }
 
-    public static Calendar get(Player player){
+    public static Calendar get(Player player) {
         return map.get(player.getUniqueId());
     }
 
-    public static boolean is(Player player){
-        if(map.containsKey(player.getUniqueId())){
+    public static boolean is(Player player) {
+        if (map.containsKey(player.getUniqueId())) {
             return true;
         }
         return false;
     }
 
-    public static void put(Player player,Calendar end){
+    public static void put(Player player, Calendar end) {
         end.add(Calendar.SECOND, getCoolDownTime());
-        map.put(player.getUniqueId(),end);
+        map.put(player.getUniqueId(), end);
     }
 
     //クールダウンの初期値を設定
-    public static int getCoolDownTime(){
+    private static int getCoolDownTime() {
         return 60;
     }
 }

4233b9700b0fdda4337d44649e232630adbc0bc7
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:41:04 +0900

    Horseコマンドの修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Horse.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Horse.java
@@ -23,50 +23,50 @@ import java.util.Arrays;
 public class Command_Horse implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            Msg.warning(sender,"Please excute this /ait command on a game!");
-            Msg.warning(sender,"/ait コマンドはゲーム内で実行してください。");
+            Msg.warning(sender, "Please excute this /ait command on a game!");
+            Msg.warning(sender, "/ait コマンドはゲーム内で実行してください。");
             return true;
         }
 
         //senderをplayerにキャスト
         Player player = (Player) sender;
 
-        if(args.length == 0){
-            help(sender,label);
+        if (args.length == 0) {
+            help(sender, label);
             return true;
         }
-        if(args[0].equalsIgnoreCase("help")){
-            help(sender,label);
+        if (args[0].equalsIgnoreCase("help")) {
+            help(sender, label);
             return true;
         }
-        if(args[0].equals("+")){
+        if (args[0].equals("+")) {
             ItemStack item = new ItemStack(Material.STICK);
             ItemMeta itemmeta = item.getItemMeta();
             itemmeta.setDisplayName(ChatColor.GOLD + "馬保護ツール");
             itemmeta.setLore(Arrays.asList(ChatColor.YELLOW + "使い方:", ChatColor.WHITE + "馬に向かって右クリックすると", ChatColor.WHITE + "馬をロックできます"));
-            itemmeta.addEnchant(Enchantment.DURABILITY , 1, true);
+            itemmeta.addEnchant(Enchantment.DURABILITY, 1, true);
             item.setItemMeta(itemmeta);
             player.getInventory().addItem(item);
             Msg.success(player, "馬保護ツールを交付しました");
             return true;
         }
-        if (args[0].equals("-")){
+        if (args[0].equals("-")) {
             ItemStack item = new ItemStack(Material.STICK);
             ItemMeta itemmeta = item.getItemMeta();
             itemmeta.setDisplayName(ChatColor.GOLD + "馬保護解除ツール");
             itemmeta.setLore(Arrays.asList(ChatColor.YELLOW + "使い方:", ChatColor.WHITE + "馬に向かって右クリックすると", ChatColor.WHITE + "馬のロックを解除できます"));
-            itemmeta.addEnchant(Enchantment.DURABILITY , 1, true);
+            itemmeta.addEnchant(Enchantment.DURABILITY, 1, true);
             item.setItemMeta(itemmeta);
             player.getInventory().addItem(item);
             Msg.success(player, "馬保護解除ツールを交付しました");
             return true;
         }
-        if(args[0].equals("?")){
+        if (args[0].equals("?")) {
             ItemStack item = new ItemStack(Material.STICK);
             ItemMeta itemmeta = item.getItemMeta();
             itemmeta.setDisplayName(ChatColor.GOLD + "馬保護情報確認ツール");
             itemmeta.setLore(Arrays.asList(ChatColor.YELLOW + "使い方:", ChatColor.WHITE + "馬に向かって右クリックすると", ChatColor.WHITE + "馬のロック情報を確認できます"));
-            itemmeta.addEnchant(Enchantment.DURABILITY , 1, true);
+            itemmeta.addEnchant(Enchantment.DURABILITY, 1, true);
             item.setItemMeta(itemmeta);
             player.getInventory().addItem(item);
             Msg.success(player, "馬保護情報確認ツールを交付しました");
@@ -75,11 +75,11 @@ public class Command_Horse implements CommandExecutor {
         return true;
     }
 
-    public void help(CommandSender sender , String commandLabel){
-        Msg.success(sender , commandLabel.toString() + " コマンドのヘルプ");
-        Msg.commandFormat(sender , commandLabel.toString() + " help", "このヘルプ");
-        Msg.commandFormat(sender , commandLabel.toString() + " +", "馬保護ツールを入手");
-        Msg.commandFormat(sender , commandLabel.toString() + " -", "馬保護解除ツールを入手");
-        Msg.commandFormat(sender , commandLabel.toString() + " ?", "馬保護情報確認ツールを入手");
+    private void help(CommandSender sender, String commandLabel) {
+        Msg.success(sender, commandLabel + " コマンドのヘルプ");
+        Msg.commandFormat(sender, commandLabel + " help", "このヘルプ");
+        Msg.commandFormat(sender, commandLabel + " +", "馬保護ツールを入手");
+        Msg.commandFormat(sender, commandLabel + " -", "馬保護解除ツールを入手");
+        Msg.commandFormat(sender, commandLabel + " ?", "馬保護情報確認ツールを入手");
     }
 }

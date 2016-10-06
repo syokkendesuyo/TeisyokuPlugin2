cdd70ce9d41edbeeadebec067556d5ba65ddca85
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:25:16 +0900

    フォーマット修正

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_SpawnEgg.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_SpawnEgg.java
@@ -11,20 +11,20 @@ import org.bukkit.inventory.ItemStack;
 
 /**
  * TeisyokuPlugin2
- *
+ * <p>
  * MOBスポナーにエッグを右クリックした時キャンセルする処理
  *
  * @auther syokkendesuyo
  */
 public class Listener_SpawnEgg implements Listener {
     @EventHandler
-    public void SpawnEgg(PlayerInteractEvent event){
+    public void SpawnEgg(PlayerInteractEvent event) {
 
         //ClickBlock
         Block block = event.getClickedBlock();
 
         //イベントの戻り値がnullだった場合の処理
-        if(!event.hasBlock()){
+        if (!event.hasBlock()) {
             return;
         }
 
@@ -39,24 +39,23 @@ public class Listener_SpawnEgg implements Listener {
 
 
         //1.Egg以外の手持ちで右クリックは無視
-        if(!(item_main.getType().toString().equals(Material.MONSTER_EGG.toString()) || item_off.getType().toString().equals(Material.MONSTER_EGG.toString()))){
+        if (!(item_main.getType().toString().equals(Material.MONSTER_EGG.toString()) || item_off.getType().toString().equals(Material.MONSTER_EGG.toString()))) {
             return;
         }
 
         //2.ターゲットがスポナー以外なら無視
-        if(!(block.getType().equals(Material.MOB_SPAWNER))){
+        if (!(block.getType().equals(Material.MOB_SPAWNER))) {
             return;
         }
 
         //teisyoku.adminを保有していた場合は利用が可能
-        if(player.hasPermission("teisyoku.admin")){
+        if (player.hasPermission("teisyoku.admin")) {
             Msg.success(player, "MOBスポナーを操作しました");
             return;
         }
 
         //3.目的に到着するのでキャンセル
-        Msg.warning(player,"MOBスポナーにモンスターエッグをクリックすることはできません");
+        Msg.warning(player, "MOBスポナーにモンスターエッグをクリックすることはできません");
         event.setCancelled(true);
-        return;
     }
 }

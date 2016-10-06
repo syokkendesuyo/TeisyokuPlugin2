8afa656ec0d2ffe5cd4e4a47c0a90258c6a6a20a
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:34:25 +0900

    Cartのコード修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Cart.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Cart.java
@@ -23,78 +23,72 @@ import java.io.IOException;
  * @auther syokkendesuyo
  */
 public class Command_Cart implements CommandExecutor {
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
-        if(!(sender instanceof Player)){
-            Msg.warning(sender,"コンソールからコマンドを送信することはできません");
+        if (!(sender instanceof Player)) {
+            Msg.warning(sender, "コンソールからコマンドを送信することはできません");
             return true;
         }
 
-        if(args.length > 2){
-            Msg.warning(sender,"引数が多すぎです");
+        if (args.length > 2) {
+            Msg.warning(sender, "引数が多すぎです");
             return true;
         }
 
-        if(args.length == 0){
-            Msg.success(sender,"マインカートをインベントリに追加しました");
-            Player player =  (Player) sender;
+        if (args.length == 0) {
+            Msg.success(sender, "マインカートをインベントリに追加しました");
+            Player player = (Player) sender;
             ItemStack cart = new ItemStack(Material.MINECART);
             player.getInventory().addItem(cart);
             return true;
-        }
-        else if(args.length == 1){
+        } else if (args.length == 1) {
             Player player = Bukkit.getServer().getPlayer(args[0]);
-            if( !(player == null) ){
-                if(!(sender.hasPermission("teisyoku.admin"))){
+            if (!(player == null)) {
+                if (!(sender.hasPermission("teisyoku.admin"))) {
                     sender.sendMessage(Messages.getNoPermissionMessage("teisyoku.admin"));
                     return true;
-                }
-                else{
-                	ItemStack cart = new ItemStack(Material.MINECART);
+                } else {
+                    ItemStack cart = new ItemStack(Material.MINECART);
                     player.getInventory().addItem(cart);
-                    Msg.success(sender,"プレイヤー " + ChatColor.YELLOW + args[0] + ChatColor.RESET + " にマインカートを渡しました");
-                    Msg.success(player,"プレイヤー " + ChatColor.YELLOW + sender.getName() + ChatColor.RESET + " からマインカートを渡されました");
+                    Msg.success(sender, "プレイヤー " + ChatColor.YELLOW + args[0] + ChatColor.RESET + " にマインカートを渡しました");
+                    Msg.success(player, "プレイヤー " + ChatColor.YELLOW + sender.getName() + ChatColor.RESET + " からマインカートを渡されました");
                     return true;
                 }
-            }
-            else{
+            } else {
                 //プレイヤーが居ないのでエラー
                 sender.sendMessage(Messages.getDenyPrefix() + "プレイヤー " + ChatColor.YELLOW + args[0] + ChatColor.RESET + " はオンラインではありません");
                 return true;
             }
-        }
-        else if(args.length == 2){
-            Player player = (Player)sender;
+        } else if (args.length == 2) {
+            Player player = (Player) sender;
             String playerUniqueId = player.getUniqueId().toString();
             File userdata = new File(TeisyokuPlugin2.getInstance().getDataFolder(), File.separator + "PlayerDatabase");
             File f = new File(userdata, File.separator + playerUniqueId + ".yml");
             FileConfiguration playerData = YamlConfiguration.loadConfiguration(f);
-            if(args[0].equalsIgnoreCase("auto_remove")){
-                if(args[1].equalsIgnoreCase("true")){
-                    try{
+            if (args[0].equalsIgnoreCase("auto_remove")) {
+                if (args[1].equalsIgnoreCase("true")) {
+                    try {
                         playerData.set("auto_cart_remove", true);
                         Msg.success(player, "マインカートから降りた時に自動削除する：" + ChatColor.GREEN + " true");
                         playerData.save(f);
-                    } catch (IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                     return true;
-                }
-                else if(args[1].equalsIgnoreCase("false")){
-                    try{
+                } else if (args[1].equalsIgnoreCase("false")) {
+                    try {
                         playerData.set("auto_cart_remove", false);
                         Msg.success(player, "マインカートから降りた時に自動削除する：" + ChatColor.RED + " false");
                         playerData.save(f);
-                    }catch (IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                     return true;
                 }
-                Msg.warning(player , "引数「" + args[1] + "」は利用できません。trueかfalseを指定して下さい。");
+                Msg.warning(player, "引数「" + args[1] + "」は利用できません。trueかfalseを指定して下さい。");
                 return true;
-            }
-            else{
-                Msg.warning(player , "引数「" + args[0] + "」は利用できません。");
+            } else {
+                Msg.warning(player, "引数「" + args[0] + "」は利用できません。");
             }
         }
         return true;

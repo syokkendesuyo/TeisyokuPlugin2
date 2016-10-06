c2a5f84608042056877a203dfbabbd61b069e2ac
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:38:11 +0900

    Flymodeコマンドの修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Flymode.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Flymode.java
@@ -14,22 +14,22 @@ import org.bukkit.entity.Player;
  * TeisyokuPlugin2
  *
  * @auther syokkendesuyo
- *
+ * <p>
  * flyコマンドを実行時の処理
  */
 public class Command_Flymode implements CommandExecutor {
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
         /**
          * コンソールからのコマンドは拒否
          */
 
-        if(! (sender  instanceof Player)){
+        if (!(sender instanceof Player)) {
             Msg.warning(sender, "コンソールからコマンドを送信することはできません");
         }
 
         //コンソール以外からの指令なのでプレイヤーと断定
-        Player player = (Player)sender;
+        Player player = (Player) sender;
 
 
         /**
@@ -37,7 +37,7 @@ public class Command_Flymode implements CommandExecutor {
          */
 
         //パーミッションの確認(コマンド側)
-        if(!(player.hasPermission(Permissions.getFlyCommandPermisson()))){
+        if (!(player.hasPermission(Permissions.getFlyCommandPermisson()))) {
             Msg.noPermissionMessage(sender, Permissions.getFlyCommandPermisson());
             return true;
         }
@@ -48,13 +48,13 @@ public class Command_Flymode implements CommandExecutor {
          */
 
         //引数が0だった場合
-        if(args.length == 0){
+        if (args.length == 0) {
             Msg.info(sender, "利用方法： /fly <true/false> (Player)");
             return true;
         }
 
         //引数が1より大きかった場合
-        else if(args.length > 2){
+        else if (args.length > 2) {
             Msg.warning(sender, "引数が多すぎです");
             Msg.warning(sender, "利用方法： /fly <true/false> (Player)");
             return true;
@@ -64,55 +64,53 @@ public class Command_Flymode implements CommandExecutor {
         /**
          * 引数が1つの場合の処理
          */
-        if(args.length ==1){
+        if (args.length == 1) {
             //引数1がtrueまたはenableだった場合flyモードを開始
-            if(args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("enable")){
+            if (args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("enable")) {
                 Listener_Flymode.enable_fly(player);
                 return true;
             }
 
             //引数1がfalseまたはdisableだった場合flyモードを終了
-            else if(args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("disable")){
+            else if (args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("disable")) {
                 Listener_Flymode.disable_fly(player);
                 return true;
             }
 
             //パーミッションの確認コマンド
-            else if(args[0].equalsIgnoreCase("perm") || args[0].equalsIgnoreCase("permission")){
+            else if (args[0].equalsIgnoreCase("perm") || args[0].equalsIgnoreCase("permission")) {
                 Msg.checkPermission(sender, Permissions.getFlyCommandPermisson());
                 Msg.checkPermission(sender, Permissions.getFlyPermisson());
                 return true;
             }
 
             //その他の場合
-            else{
+            else {
                 Msg.warning(sender, "引数「" + args[0] + "」は存在しません");
                 Msg.warning(sender, "利用方法： /fly <true/false> (Player)");
                 return true;
             }
-        }
-        else if(args.length == 2){
+        } else if (args.length == 2) {
             Player target_player = Bukkit.getServer().getPlayer(args[1]);
-            if( !(target_player == null) ){
+            if (!(target_player == null)) {
                 //正常に処理
-                if(args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("enable")){
+                if (args[0].equalsIgnoreCase("true") || args[0].equalsIgnoreCase("enable")) {
                     Listener_Flymode.enable_fly(target_player);
                     return true;
                 }
 
                 //引数1がfalseまたはdisableだった場合flyモードを終了
-                else if(args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("disable")){
+                else if (args[0].equalsIgnoreCase("false") || args[0].equalsIgnoreCase("disable")) {
                     Listener_Flymode.disable_fly(target_player);
                     return true;
                 }
                 //その他の場合
-                else{
+                else {
                     Msg.warning(sender, "引数「" + args[0] + "」は存在しません");
                     Msg.warning(sender, "利用方法： /fly <true/false> (Player)");
                     return true;
                 }
-            }
-            else{
+            } else {
                 //プレイヤーが居ないのでエラー
                 Msg.warning(sender, "プレイヤー " + ChatColor.YELLOW + args[1] + ChatColor.RESET + " はオンラインではありません");
                 return true;

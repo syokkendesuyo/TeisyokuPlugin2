184b6a92db8f0ad0cb8f8d94f18b49a73c503c4b
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:04:06 +0900

    軽微な修正

--- src/main/java/net/jp/minecraft/plugins/GUI/GUI.java
+++ src/main/java/net/jp/minecraft/plugins/GUI/GUI.java
@@ -13,18 +13,19 @@ import org.bukkit.inventory.ItemStack;
 public class GUI {
     /**
      * CreateInventoryをちょっと使いやすくしたメソッド
+     *
      * @param player
      * @param inv_name
      * @param size
      * @param items
      * @return
      */
-    public static Inventory create(Player player, int size, String inv_name, ItemStack... items){
+    public static Inventory create(Player player, int size, String inv_name, ItemStack... items) {
         //Define inv
         Inventory inv = Bukkit.createInventory(player, size * 9, inv_name);
 
         //Set items
-        for (int cnt = 0; cnt < items.length; cnt++){
+        for (int cnt = 0; cnt < items.length; cnt++) {
             inv.setItem(cnt, items[cnt]);
         }
         return inv;

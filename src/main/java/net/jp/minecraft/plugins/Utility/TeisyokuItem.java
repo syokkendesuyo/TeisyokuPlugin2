fb9afd6d5145363c1ac19fcef3cb3729716868bc
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:45:42 +0900

    軽微な修正

--- src/main/java/net/jp/minecraft/plugins/Utility/TeisyokuItem.java
+++ src/main/java/net/jp/minecraft/plugins/Utility/TeisyokuItem.java
@@ -17,37 +17,38 @@ public class TeisyokuItem {
      * <p>アイテムのカスタマイズを行う関数<br/>
      * GUIに置くアイテムのカスタマイズに重宝します<br/>
      * 注意：String loreの配列が3つ以上になるとloreにエラーが表示されます</p>
+     *
      * @param display_name 表示名(必須 String)
-     * @param stack スタック数(必須 int)
-     * @param material 素材名(必須 Material)
-     * @param meta メタ番号(必須 Short)
-     * @param lore 詳細(任意 String ※配列)
+     * @param stack        スタック数(必須 int)
+     * @param material     素材名(必須 Material)
+     * @param meta         メタ番号(必須 Short)
+     * @param lore         詳細(任意 String ※配列)
      * @return アイテム
      */
-    public static ItemStack custom_item(String display_name, Integer stack, Material material, Short meta, String... lore){
+    public static ItemStack custom_item(String display_name, Integer stack, Material material, Short meta, String... lore) {
         org.bukkit.inventory.ItemStack item = new org.bukkit.inventory.ItemStack(material, stack, meta);
         ItemMeta itemmeta = item.getItemMeta();
         itemmeta.setDisplayName(display_name);
-        if(lore.length == 0){
+        if (lore.length == 0) {
             item.setItemMeta(itemmeta);
             return item;
         }
-        if (lore.length == 1){
+        if (lore.length == 1) {
             itemmeta.setLore(Arrays.asList(ChatColor.WHITE + lore[0]));
             item.setItemMeta(itemmeta);
             return item;
         }
-        if (lore.length == 2){
-            itemmeta.setLore(Arrays.asList(ChatColor.WHITE + lore[0],ChatColor.WHITE + lore[1]));
+        if (lore.length == 2) {
+            itemmeta.setLore(Arrays.asList(ChatColor.WHITE + lore[0], ChatColor.WHITE + lore[1]));
             item.setItemMeta(itemmeta);
             return item;
         }
-        if(lore.length == 3){
-            itemmeta.setLore(Arrays.asList(ChatColor.WHITE + lore[0],ChatColor.WHITE + lore[1],ChatColor.WHITE + lore[2]));
+        if (lore.length == 3) {
+            itemmeta.setLore(Arrays.asList(ChatColor.WHITE + lore[0], ChatColor.WHITE + lore[1], ChatColor.WHITE + lore[2]));
             item.setItemMeta(itemmeta);
             return item;
         }
-        itemmeta.setLore(Arrays.asList("Error:TeisyokuItem.java","loreは3つまでしか登録できません。配列を確認してください。"));
+        itemmeta.setLore(Arrays.asList("Error:TeisyokuItem.java", "loreは3つまでしか登録できません。配列を確認してください。"));
         item.setItemMeta(itemmeta);
         return item;
     }

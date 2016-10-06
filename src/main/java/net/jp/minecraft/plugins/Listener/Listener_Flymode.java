0c95b179cc9ff746d7bd174ed905563b08096a03
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:15:33 +0900

    FlyModeの修正

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_Flymode.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_Flymode.java
@@ -8,22 +8,22 @@ import org.bukkit.entity.Player;
  * TeisyokuPlugin2
  *
  * @auther syokkendesuyo
- *
+ * <p>
  * flyの実行時の処理
  */
 public class Listener_Flymode {
 
     //flyを有効化
-    public static boolean enable_fly(Player player){
-        Msg.success(player, ChatColor.YELLOW + player.getName().toString() + ChatColor.RESET + " のFlyモードを" + ChatColor.GREEN + " 有効 " + ChatColor.RESET + "にしました");
+    public static boolean enable_fly(Player player) {
+        Msg.success(player, ChatColor.YELLOW + player.getName() + ChatColor.RESET + " のFlyモードを" + ChatColor.GREEN + " 有効 " + ChatColor.RESET + "にしました");
         player.setAllowFlight(true);
         return true;
     }
 
 
     //flyを無効化
-    public static boolean disable_fly(Player player){
-        Msg.success(player, ChatColor.YELLOW + player.getName().toString() + ChatColor.RESET + " のFlyモードを" + ChatColor.RED + " 無効 " + ChatColor.RESET + "にしました");
+    public static boolean disable_fly(Player player) {
+        Msg.success(player, ChatColor.YELLOW + player.getName() + ChatColor.RESET + " のFlyモードを" + ChatColor.RED + " 無効 " + ChatColor.RESET + "にしました");
         player.setAllowFlight(false);
         return true;
     }

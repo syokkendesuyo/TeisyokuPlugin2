43bf6a359f70a0fad5be74b44c6d151c6b1ab1dc
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:39:23 +0900

    Gomiコマンドの修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Gomibako.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Gomibako.java
@@ -15,21 +15,19 @@ import org.bukkit.command.CommandSender;
 public class Command_Gomibako implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if(! (sender.hasPermission(Permissions.getGomibakoCommandPermisson()))){
-            Msg.noPermissionMessage(sender,Permissions.getGomibakoCommandPermisson());
+        if (!(sender.hasPermission(Permissions.getGomibakoCommandPermisson()))) {
+            Msg.noPermissionMessage(sender, Permissions.getGomibakoCommandPermisson());
             return true;
         }
 
-        if(args.length != 1){
+        if (args.length != 1) {
             Listener_Gomibako.openGomibako(sender);
-        }
-        else if(args.length == 1){
-            if(args[0].equalsIgnoreCase("perm")||args[0].equalsIgnoreCase("permission")){
+        } else {
+            if (args[0].equalsIgnoreCase("perm") || args[0].equalsIgnoreCase("permission")) {
                 Msg.info(sender, "パーミッション(通常利用): " + Permissions.getGomibakoPermisson());
                 Msg.info(sender, "パーミッション(コマンド): " + Permissions.getGomibakoCommandPermisson());
-            }
-           else{
-                Msg.warning(sender,"引数「" + args[0] + "」は存在しません");
+            } else {
+                Msg.warning(sender, "引数「" + args[0] + "」は存在しません");
             }
         }
         return true;

5d666993e557e91a90c4886fdcfcacd5fe8cb19e
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:52:07 +0900

    軽微な修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_SignEdit.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_SignEdit.java
@@ -1,14 +1,9 @@
 package net.jp.minecraft.plugins.Commands;
 
-import net.jp.minecraft.plugins.GUI.GUI_Anvil;
-import net.jp.minecraft.plugins.Utility.TeisyokuItem;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 /**
  * TeisyokuPlugin2

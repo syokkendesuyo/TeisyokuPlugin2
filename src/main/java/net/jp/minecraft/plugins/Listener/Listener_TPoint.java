d9c19e39ff2b18f0d2ce3d1636cdb7779b5ccc33
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:27:08 +0900

    軽微な修正

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_TPoint.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_TPoint.java
@@ -24,12 +24,13 @@ public class Listener_TPoint {
 
     /**
      * プレイヤーのポイントにポイントを追加します<br />
+     *
      * @param point
      * @param target_player
      * @param sender
      */
-    public static Boolean addPoint(int point , Player target_player , CommandSender sender){
-        try{
+    public static Boolean addPoint(int point, Player target_player, CommandSender sender) {
+        try {
             cfile = new File(df, "PlayerDatabase" + File.separator + target_player.getUniqueId() + ".yml");
             config = YamlConfiguration.loadConfiguration(cfile);
             FileConfiguration playerData = config;
@@ -37,41 +38,40 @@ public class Listener_TPoint {
             int point_after = point_before + point;
             playerData.set("tpoint", point_after);
             save();
-            Msg.success(target_player, "" + ChatColor.AQUA + ChatColor.BOLD + point + " TPoint"+ ChatColor.RESET +" 受け取りました");
+            Msg.success(target_player, "" + ChatColor.AQUA + ChatColor.BOLD + point + " TPoint" + ChatColor.RESET + " 受け取りました");
             Msg.success(sender, target_player.getName() + " さんに " + point + " TPoint与えました");
             status(target_player);//ステイタスを表示
             return true;
-        }
-        catch (Exception e){
-            Msg.warning(target_player,"不明なエラーが発生しました。 Location: Listener_TPoint >> addPoint");
-            Msg.warning(sender,"不明なエラーが発生しました。 Location: Listener_TPoint >> addPoint");
+        } catch (Exception e) {
+            Msg.warning(target_player, "不明なエラーが発生しました。 Location: Listener_TPoint >> addPoint");
+            Msg.warning(sender, "不明なエラーが発生しました。 Location: Listener_TPoint >> addPoint");
             return false;
         }
     }
 
     /**
      * プレイヤーのポイントを差し引きます<br />
+     *
      * @param point
      * @param target_player
      * @param sender
      * @return 成功・不成功の結果を返却
      */
-    public static boolean subtractPoint(int point , Player target_player , CommandSender sender){
-        try{
+    public static boolean subtractPoint(int point, Player target_player, CommandSender sender) {
+        try {
             cfile = new File(df, "PlayerDatabase" + File.separator + target_player.getUniqueId() + ".yml");
             config = YamlConfiguration.loadConfiguration(cfile);
             FileConfiguration playerData = config;
             int point_before = playerData.getInt("tpoint");
             int point_after = point_before - point;
 
-            if(point_after < 0){
+            if (point_after < 0) {
                 int error = Math.abs(point_after);
                 Msg.warning(target_player, point + " TPoint消費しようとしましたが、" + error + " TPoint足りませんでした");
-                Msg.warning(sender,  ChatColor.YELLOW + target_player.getName() + ChatColor.RESET + " さんから " + point + " TPoint消費しようとしましたが、" + error + " TPoint足りませんでした");
+                Msg.warning(sender, ChatColor.YELLOW + target_player.getName() + ChatColor.RESET + " さんから " + point + " TPoint消費しようとしましたが、" + error + " TPoint足りませんでした");
                 status(target_player);//ステイタスを表示
                 return false;
-            }
-            else{
+            } else {
                 playerData.set("tpoint", point_after);
                 save();
                 Msg.success(target_player, point + " TPoint消費しました");
@@ -79,117 +79,112 @@ public class Listener_TPoint {
                 status(target_player);//ステイタスを表示
                 return true;
             }
-        }
-        catch (Exception e){
-            Msg.warning(target_player,"不明なエラーが発生しました。 Location: Listener_TPoint >> subtractPoint");
-            Msg.warning(sender,"不明なエラーが発生しました。 Location: Listener_TPoint >> subtractPoint");
+        } catch (Exception e) {
+            Msg.warning(target_player, "不明なエラーが発生しました。 Location: Listener_TPoint >> subtractPoint");
+            Msg.warning(sender, "不明なエラーが発生しました。 Location: Listener_TPoint >> subtractPoint");
             return false;
         }
     }
 
-    public static boolean canBuy(int point , Player player){
+    public static boolean canBuy(int point, Player player) {
         cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
         config = YamlConfiguration.loadConfiguration(cfile);
         FileConfiguration playerData = config;
         int point_before = playerData.getInt("tpoint");
         int point_after = point_before - point;
 
-        if(point_after < 0){
+        if (point_after < 0) {
             return false;
-        }
-        else{
+        } else {
             return true;
         }
     }
 
     /**
      * プレイヤーのポイントをセットします<br />
+     *
      * @param point
      * @param player
      */
-    public static void setPoint(int point , Player player){
+    public static void setPoint(int point, Player player) {
         cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
         config = YamlConfiguration.loadConfiguration(cfile);
         FileConfiguration playerData = config;
         playerData.set("tpoint", point);
-        Msg.success(player," TPointにセットしました");
+        Msg.success(player, " TPointにセットしました");
         save();
         status(player);//ステイタスを表示
     }
 
     /**
      * Playerを渡すことで現在の保有ポイントを取得できます。<br />
+     *
      * @param player
      */
-    public static void status(Player player){
-        try{
+    public static void status(Player player) {
+        try {
             //正常に取得
             cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
             config = YamlConfiguration.loadConfiguration(cfile);
             FileConfiguration playerData = config;
             int point = playerData.getInt("tpoint");
-            int syokken = point /1010;
+            int syokken = point / 1010;
             Msg.info(player, "現在の保有ポイント： " + ChatColor.AQUA + ChatColor.BOLD + point + " TPoint");
-            Msg.info(player, "食券換算 : " + ChatColor.GREEN + ChatColor.BOLD + syokken + "枚" + ChatColor.RESET +"分");
-            return;
-        }
-        catch (Exception e){
+            Msg.info(player, "食券換算 : " + ChatColor.GREEN + ChatColor.BOLD + syokken + "枚" + ChatColor.RESET + "分");
+        } catch (Exception e) {
             //エラー
             Msg.warning(player, "ポイント取得時にエラーが発生しました。管理者に以下のエラーをお知らせください。");
             Msg.warning(player, "Exception Error : Listener_TPoint.status function");
             e.printStackTrace();
-            return;
         }
     }
 
     /**
      * uuidとplayer(sender),string(target)を渡すことで現在の保有ポイントを取得できます。<br />
+     *
      * @param uuid
      * @param sender
      * @param target
      * @param player
      */
-    public static void status_uuid(UUID uuid , CommandSender sender, String target, Player player){
-        try{
+    public static void status_uuid(UUID uuid, CommandSender sender, String target, Player player) {
+        try {
             //正常に取得
             cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
             config = YamlConfiguration.loadConfiguration(cfile);
             FileConfiguration playerData = config;
-            if(!cfile.exists()){
-                Msg.warning(player, ChatColor.YELLOW + target.toString() +ChatColor.RESET + " は見つかりませんでした。");
+            if (!cfile.exists()) {
+                Msg.warning(player, ChatColor.YELLOW + target + ChatColor.RESET + " は見つかりませんでした。");
                 return;
             }
 
             int point = playerData.getInt("tpoint");
-            int syokken = point /1010;
-            Msg.info(sender, ChatColor.YELLOW + target.toString() +ChatColor.RESET + " さんの保有ポイント： " + ChatColor.AQUA + ChatColor.BOLD + point + " TPoint ");
-            Msg.info(sender, "食券換算 : " + ChatColor.GREEN + ChatColor.BOLD + syokken + "枚" + ChatColor.RESET +"分");
-            return;
-        }
-        catch (Exception e){
+            int syokken = point / 1010;
+            Msg.info(sender, ChatColor.YELLOW + target + ChatColor.RESET + " さんの保有ポイント： " + ChatColor.AQUA + ChatColor.BOLD + point + " TPoint ");
+            Msg.info(sender, "食券換算 : " + ChatColor.GREEN + ChatColor.BOLD + syokken + "枚" + ChatColor.RESET + "分");
+        } catch (Exception e) {
             //エラー
             Msg.warning(sender, "ポイント取得時にエラーが発生しました。管理者に以下のエラーをお知らせください。");
             Msg.warning(sender, "Exception Error : Listener_TPoint.status function");
             e.printStackTrace();
-            return;
         }
     }
 
     /**
      * Playerを渡すことで現在保有しているポイントをintで返します<br />
+     *
      * @param player
      * @return 保有ポイント(int)
      */
-    public static int int_status(Player player){
-        try{
+    public static int int_status(Player player) {
+        try {
             //正常に取得
             cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
             config = YamlConfiguration.loadConfiguration(cfile);
             FileConfiguration playerData = config;
             int point = playerData.getInt("tpoint");
             return point;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             //エラー
             Msg.warning(player, "ポイント取得時にエラーが発生しました。管理者に以下のエラーをお知らせください。");
             Msg.warning(player, "Exception Error : Listener_TPoint.int_status function");
@@ -210,20 +205,20 @@ public class Listener_TPoint {
     public static void save() {
         try {
             config.save(cfile);
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void color_default(Player player){
+    public static void color_default(Player player) {
         cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
         config = YamlConfiguration.loadConfiguration(cfile);
         FileConfiguration playerData = config;
-        playerData.set("nick_color","default");
+        playerData.set("nick_color", "default");
         save();
     }
 
-    public static void color_aqua(Player player){
+    public static void color_aqua(Player player) {
         cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
         config = YamlConfiguration.loadConfiguration(cfile);
         FileConfiguration playerData = config;
@@ -231,7 +226,7 @@ public class Listener_TPoint {
         save();
     }
 
-    public static void color_pink(Player player){
+    public static void color_pink(Player player) {
         cfile = new File(df, "PlayerDatabase" + File.separator + player.getUniqueId() + ".yml");
         config = YamlConfiguration.loadConfiguration(cfile);
         FileConfiguration playerData = config;
--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_TicksPerSecond_1_9_R1.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_TicksPerSecond_1_9_R1.java
@@ -12,22 +12,21 @@ import java.math.BigDecimal;
  */
 public class Listener_TicksPerSecond_1_9_R1 {
 
-    public static String getTpsString(){
-        StringBuilder sb = new StringBuilder( ChatColor.GOLD + "TPS from last 1m, 5m, 15m: " );
-        for ( double tps : MinecraftServer.getServer().recentTps )
-        {
-            sb.append( tps );
-            sb.append( ", " );
+    public static String getTpsString() {
+        StringBuilder sb = new StringBuilder(ChatColor.GOLD + "TPS from last 1m, 5m, 15m: ");
+        for (double tps : MinecraftServer.getServer().recentTps) {
+            sb.append(tps);
+            sb.append(", ");
         }
-        return sb.substring( 0, sb.length() - 2);
+        return sb.substring(0, sb.length() - 2);
     }
 
-    public static double getTps(int num){
+    public static double getTps(int num) {
         double tps[] = MinecraftServer.getServer().recentTps;
-        return tps[num-1];
+        return tps[num - 1];
     }
 
-    public static String doubleToString(double num){
+    public static String doubleToString(double num) {
         BigDecimal bd = new BigDecimal(num);
         BigDecimal bd2 = bd.setScale(3, BigDecimal.ROUND_HALF_UP);
         return bd2.toString();

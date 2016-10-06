243c1e5d1ff78c76d504b203c57c666f194f8a2e
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:36:12 +0900

    Dauniiのコード修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Daunii.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Daunii.java
@@ -1,5 +1,8 @@
 package net.jp.minecraft.plugins.Commands;
 
+import net.jp.minecraft.plugins.Listener.Listener_Daunii_1_9_R1;
+import net.jp.minecraft.plugins.Messages;
+import net.jp.minecraft.plugins.Utility.Msg;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,27 +12,23 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.IronGolem;
 import org.bukkit.entity.Player;
 
-import net.jp.minecraft.plugins.Messages;
-import net.jp.minecraft.plugins.Listener.Listener_Daunii_1_9_R1;
-import net.jp.minecraft.plugins.Utility.Msg;
+public class Command_Daunii implements CommandExecutor {
 
-public class Command_Daunii  implements CommandExecutor {
-    
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args){
+    public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args) {
         
-        if(!(sender instanceof Player)){
-            Msg.warning(sender,"コンソールからコマンドを送信することはできません");
+        if (!(sender instanceof Player)) {
+            Msg.warning(sender, "コンソールからコマンドを送信することはできません");
             return false;
         }
-        
-        if(!(sender.hasPermission("teisyoku.admin"))){
+
+        if (!(sender.hasPermission("teisyoku.admin"))) {
             sender.sendMessage(Messages.getNoPermissionMessage("teisyoku.admin"));
             return false;
         }
-        
-        if(!(args.length == 0)){
-            Msg.warning(sender,"引数が多すぎです");
+
+        if (!(args.length == 0)) {
+            Msg.warning(sender, "引数が多すぎです");
             return false;
         }
         Player player = (Player) sender;
@@ -37,19 +36,19 @@ public class Command_Daunii  implements CommandExecutor {
         Daunii.setCustomName(Listener_Daunii_1_9_R1.DauniiName);
         Daunii.setCustomNameVisible(true);
         Daunii.setRemoveWhenFarAway(false);
-        
+
         Location loc = player.getLocation();
-        
+
         ArmorStand setDaunii = (ArmorStand) player.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
         setDaunii.setMarker(true);
         setDaunii.setGravity(false);
         setDaunii.setSmall(true);
         setDaunii.setVisible(false);
         setDaunii.setPassenger(Daunii);
-        
-        Msg.success(player,"だうにーくんを召喚しました");
+
+        Msg.success(player, "だうにーくんを召喚しました");
         return true;
-        
+
     }
 
 }

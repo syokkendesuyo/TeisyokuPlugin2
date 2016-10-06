b842b5c5e9c3db8c64a92d92bfd416d2fda53153
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:16:00 +0900

    フォーマット修正

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_Gomibako.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_Gomibako.java
@@ -20,27 +20,27 @@ import java.util.Arrays;
  * @auther syokkendesuyo
  */
 public class Listener_Gomibako implements Listener {
-    public static void openGomibako(CommandSender sender){
+    public static void openGomibako(CommandSender sender) {
         if (!(sender instanceof Player)) {
             Msg.warning(sender, "ゴミ箱コマンドはゲーム内からのみ実行できます");
         }
 
         Player player = (Player) sender;
 
-        if(! (player.hasPermission(Permissions.getGomibakoPermisson()))){
+        if (!(player.hasPermission(Permissions.getGomibakoPermisson()))) {
             Msg.noPermissionMessage(sender, Permissions.getGomibakoPermisson());
             return;
         }
 
         ItemStack item0 = new ItemStack(Material.BOOK);
         ItemMeta itemmeta0 = item0.getItemMeta();
-        itemmeta0.setDisplayName(ChatColor.GOLD  + "ゴミ箱の使い方");
+        itemmeta0.setDisplayName(ChatColor.GOLD + "ゴミ箱の使い方");
         itemmeta0.setLore(Arrays.asList(ChatColor.WHITE + "ゴミ箱に不要なアイテムを収納し、", ChatColor.WHITE + "画面を閉じると処分が完了します。"));
         item0.setItemMeta(itemmeta0);
 
         ItemStack item1 = new ItemStack(Material.BED);
         ItemMeta itemmeta1 = item1.getItemMeta();
-        itemmeta1.setDisplayName(ChatColor.GOLD  + "画面を閉じる");
+        itemmeta1.setDisplayName(ChatColor.GOLD + "画面を閉じる");
         itemmeta1.setLore(Arrays.asList(ChatColor.GRAY + "定食サーバオリジナルプラグイン！"));
         item1.setItemMeta(itemmeta1);
 

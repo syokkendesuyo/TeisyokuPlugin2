b1d105a69ab9c58a3e5f71470ef72af159e06b1a
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 01:45:31 +0900

    Playersコマンドの最適化

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Players.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Players.java
@@ -18,25 +18,25 @@ public class Command_Players implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if(!(sender.hasPermission(Permissions.getPlayersCommandPermisson()))){
+        if (!(sender.hasPermission(Permissions.getPlayersCommandPermisson()))) {
             Msg.noPermissionMessage(sender, Permissions.getPlayersCommandPermisson());
             return true;
         }
 
-        if(! (sender  instanceof Player)){
+        if (!(sender instanceof Player)) {
             Msg.warning(sender, "コンソールからコマンドを送信することはできません");
             return true;
         }
 
-        Player player = (Player)sender;
+        Player player = (Player) sender;
 
-        if(args.length == 0){
+        if (args.length == 0) {
             GUI_PlayersList.getPlayersList(player);
             return true;
         }
 
-        if(args.length == 1){
-            if(!(args[0].equalsIgnoreCase("total"))){
+        if (args.length == 1) {
+            if (!(args[0].equalsIgnoreCase("total"))) {
                 GUI_PlayersList.getPlayersList(player);
                 return true;
             }

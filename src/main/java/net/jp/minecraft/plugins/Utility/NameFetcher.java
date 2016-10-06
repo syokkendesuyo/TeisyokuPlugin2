4e3cae73a1639b0943b2011aac8627667c71a4f4
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:35:50 +0900

    NF修正

--- src/main/java/net/jp/minecraft/plugins/Utility/NameFetcher.java
+++ src/main/java/net/jp/minecraft/plugins/Utility/NameFetcher.java
@@ -37,15 +37,17 @@ import java.util.concurrent.Callable;
 
 /**
  * TeisyokuPlugin2
- *
+ * 
  * Thanks!
  * Gist:https://gist.github.com/evilmidget38/26d70114b834f71fb3b4
+ *
  * @author evilmidget38 (Nate Mortensen)
  */
 public class NameFetcher implements Callable<Map<UUID, String>> {
     private static final String PROFILE_URL = "https://sessionserver.mojang.com/session/minecraft/profile/";
     private final JSONParser jsonParser = new JSONParser();
     private final List<UUID> uuids;
+
     public NameFetcher(List<UUID> uuids) {
         this.uuids = ImmutableList.copyOf(uuids);
     }
@@ -53,8 +55,8 @@ public class NameFetcher implements Callable<Map<UUID, String>> {
     @Override
     public Map<UUID, String> call() throws Exception {
         Map<UUID, String> uuidStringMap = new HashMap<>();
-        for (UUID uuid: uuids) {
-            HttpURLConnection connection = (HttpURLConnection) new URL(PROFILE_URL+uuid.toString().replace("-", "")).openConnection();
+        for (UUID uuid : uuids) {
+            HttpURLConnection connection = (HttpURLConnection) new URL(PROFILE_URL + uuid.toString().replace("-", "")).openConnection();
             JSONObject response = (JSONObject) jsonParser.parse(new InputStreamReader(connection.getInputStream()));
             String name = (String) response.get("name");
             if (name == null) {

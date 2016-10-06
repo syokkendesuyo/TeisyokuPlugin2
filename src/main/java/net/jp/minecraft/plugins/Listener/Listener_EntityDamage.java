47e01ea13d46bb71c696db9748fdfcae3a682106
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:14:58 +0900

    EntityDamageの修正

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_EntityDamage.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_EntityDamage.java
@@ -1,10 +1,12 @@
 package net.jp.minecraft.plugins.Listener;
 
-import net.jp.minecraft.plugins.Listener.Listener_Horse;
 import net.jp.minecraft.plugins.Utility.Msg;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -19,12 +21,12 @@ import java.util.UUID;
 public class Listener_EntityDamage implements Listener {
 
     @EventHandler
-    public void EntityDamage(EntityDamageByEntityEvent event){
+    public void EntityDamage(EntityDamageByEntityEvent event) {
         Entity damageEntity = event.getEntity();
         Entity damager = event.getDamager();
 
         //ダメージを与えたのがプレイヤー以外ならさよなら
-        if(!(damager instanceof Player)){
+        if (!(damager instanceof Player)) {
             return;
         }
 
@@ -32,15 +34,15 @@ public class Listener_EntityDamage implements Listener {
         Player player = (Player) damager;
 
         //ダメージを受けたのが村人か馬じゃなかったらさよなら
-        if(!(damageEntity instanceof Villager || damageEntity instanceof Horse)){
+        if (!(damageEntity instanceof Villager || damageEntity instanceof Horse)) {
             return;
         }
 
-        if(player.hasPermission("teisyoku.admin")){
+        if (player.hasPermission("teisyoku.admin")) {
             return;
         }
 
-        if(player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SWORD){
+        if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SWORD) {
             return;
         }
 
@@ -50,12 +52,12 @@ public class Listener_EntityDamage implements Listener {
     }
 
     @EventHandler
-    public void HorseDamage(EntityDamageByEntityEvent event){
+    public void HorseDamage(EntityDamageByEntityEvent event) {
         Entity damageEntity = event.getEntity();
         Entity damager = event.getDamager();
 
         //ダメージを与えたのがプレイヤー以外ならさよなら
-        if(!(damager instanceof Player)){
+        if (!(damager instanceof Player)) {
             return;
         }
 
@@ -63,23 +65,22 @@ public class Listener_EntityDamage implements Listener {
         Player player = (Player) damager;
 
         //ダメージを受けたのが村人か馬じゃなかったらさよなら
-        if(!(damageEntity instanceof Horse)){
+        if (!(damageEntity instanceof Horse)) {
             return;
         }
 
-        if(player.hasPermission("teisyoku.admin")){
+        if (player.hasPermission("teisyoku.admin")) {
             return;
         }
 
         //ツールではダメージが通らないように
-        if(! player.getInventory().getItemInMainHand().getType().equals(Material.AIR)){
-            try{
-                if(player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "馬保護ツール") || player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "馬保護解除ツール") || player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "馬保護情報確認ツール")){
+        if (!player.getInventory().getItemInMainHand().getType().equals(Material.AIR)) {
+            try {
+                if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "馬保護ツール") || player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "馬保護解除ツール") || player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.GOLD + "馬保護情報確認ツール")) {
                     event.setCancelled(true);
                     return;
                 }
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 //謎のエラー回避
             }
         }
@@ -88,19 +89,13 @@ public class Listener_EntityDamage implements Listener {
         UUID entityUUID = event.getEntity().getUniqueId();
 
         int temp = Listener_Horse.isEqual(player, playerUUID, entityUUID);
-        if(temp == 1){
+        if (temp == 1) {
             Msg.warning(player, "ロックされている為ダメージを与えることはできません");
             event.setCancelled(true);
-            return;
-        }
-        else if(temp == 2){
-            return;
-        }
-        else {
+        } else {
             Msg.warning(player, "ロックされている為ダメージを与えることはできません");
             Listener_Horse.getStatus(player, entityUUID);
             event.setCancelled(true);
-            return;
         }
     }
 }

df15d92894b04be2b07805f1458291d515736768
syokkendesuyo <syokkendesuyo@gmail.com>
2016-10-07 02:44:17 +0900

    Soundsの修正

--- src/main/java/net/jp/minecraft/plugins/Utility/Sounds.java
+++ src/main/java/net/jp/minecraft/plugins/Utility/Sounds.java
@@ -10,51 +10,43 @@ import org.bukkit.event.Listener;
  *
  * @auther syokkendesuyo
  */
-public class Sounds implements Listener{
-    public static String ver1_8_8_R01 = "1.8.8-R0.1-SNAPSHOT";
-    public static String ver1_9_2_R01 = "1.9.2-R0.1-SNAPSHOT";
+public class Sounds implements Listener {
+    private static String ver1_8_8_R01 = "1.8.8-R0.1-SNAPSHOT";
+    private static String ver1_9_2_R01 = "1.9.2-R0.1-SNAPSHOT";
 
-    public static void sound_note(Player player){
+    public static void sound_note(Player player) {
         String version = Bukkit.getBukkitVersion();
         if (version.equals(ver1_8_8_R01)) {
             return;
-        }
-        else if(version.equals(ver1_9_2_R01)){
+        } else if (version.equals(ver1_9_2_R01)) {
             player.playSound(player.getLocation(), org.bukkit.Sound.BLOCK_NOTE_PLING, 3.0F, 1.5F);
-            return;
         }
     }
 
-    public static void sound_arrow(Player player){
+    public static void sound_arrow(Player player) {
         String version = Bukkit.getBukkitVersion();
         if (version.equals(ver1_8_8_R01)) {
             return;
-        }
-        else if(version.equals(ver1_9_2_R01)){
+        } else if (version.equals(ver1_9_2_R01)) {
             player.playSound(player.getLocation(), Sound.ENTITY_ARROW_SHOOT, 10.0F, 1.0F);
-            return;
         }
     }
 
-    public static void sound_villager(Player player){
+    public static void sound_villager(Player player) {
         String version = Bukkit.getBukkitVersion();
         if (version.equals(ver1_8_8_R01)) {
             return;
-        }
-        else if(version.equals(ver1_9_2_R01)){
+        } else if (version.equals(ver1_9_2_R01)) {
             player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_DEATH, 10.0F, 1.0F);
-            return;
         }
     }
 
-    public static void sound_zombie_villager(Player player){
+    public static void sound_zombie_villager(Player player) {
         String version = Bukkit.getBukkitVersion();
         if (version.equals(ver1_8_8_R01)) {
             return;
-        }
-        else if(version.equals(ver1_9_2_R01)){
+        } else if (version.equals(ver1_9_2_R01)) {
             player.playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_VILLAGER_CURE, 1.0F, 1.0F);
-            return;
         }
     }
 }

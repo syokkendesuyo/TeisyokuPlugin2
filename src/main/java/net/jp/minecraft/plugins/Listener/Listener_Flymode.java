90b4bb4e9a2ee1e48dfdf86382feff0b0b0b2ec7
syokkendesuyo <syokkendesuyo@gmail.com>
2017-03-03 19:25:14 +0900

    flatワールドでflyモードがONになるように修正

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_Flymode.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_Flymode.java
@@ -1,8 +1,13 @@
 package net.jp.minecraft.plugins.Listener;
 
+import net.jp.minecraft.plugins.Permissions;
 import net.jp.minecraft.plugins.Utility.Msg;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 /**
  * TeisyokuPlugin2
@@ -11,7 +16,7 @@ import org.bukkit.entity.Player;
  * <p>
  * flyの実行時の処理
  */
-public class Listener_Flymode {
+public class Listener_Flymode implements Listener{
 
     //flyを有効化
     public static boolean enable_fly(Player player) {
@@ -20,11 +25,27 @@ public class Listener_Flymode {
         return true;
     }
 
-
     //flyを無効化
     public static boolean disable_fly(Player player) {
         Msg.success(player, ChatColor.YELLOW + player.getName() + ChatColor.RESET + " のFlyモードを" + ChatColor.RED + " 無効 " + ChatColor.RESET + "にしました");
         player.setAllowFlight(false);
         return true;
+    }
+
+    @EventHandler
+    public void onPlayerChangedWorldEvent (PlayerChangedWorldEvent event) {
+        if ( event.getPlayer().hasPermission(Permissions.getTeisyokuAdminPermisson())) {
+            return;
+        }
+        event.getPlayer().setAllowFlight(false);
+    }
+
+    @EventHandler (priority = EventPriority.LOWEST)
+    public void onPlayerFlatWorldEnterEvent (PlayerChangedWorldEvent event)  {
+        if (event.getPlayer().getWorld().getName().equalsIgnoreCase("flat")) {
+            enable_fly(event.getPlayer());
+            return;
+        }
+        event.getPlayer().setAllowFlight(false);
     }
 }

1463cb68078a568273563147f185fa8215c138e5
syokkendesuyo <syokkendesuyo@gmail.com>
2017-03-05 01:19:54 +0900

    ログイン時の音声エフェクトを削除

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_JoinEvent.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_JoinEvent.java
@@ -3,9 +3,7 @@ package net.jp.minecraft.plugins.Listener;
 import net.jp.minecraft.plugins.API.API_Discord;
 import net.jp.minecraft.plugins.Messages;
 import net.jp.minecraft.plugins.TeisyokuPlugin2;
-import net.jp.minecraft.plugins.Utility.Sounds;
 import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,18 +16,9 @@ import java.util.List;
  */
 public class Listener_JoinEvent implements Listener {
 
-    private String ver1_8_8_R01 = "1.8.8-R0.1-SNAPSHOT";
-    private String ver1_9_2_R01 = "1.9.2-R0.1-SNAPSHOT";
-
     @EventHandler
     public void onPlayerJoinMessage(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        String version = Bukkit.getBukkitVersion();
-        if (version.equals(ver1_8_8_R01)) {
-            Sounds.sound_villager(player);
-        } else if (version.equals(ver1_9_2_R01)) {
-            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_DEATH, 10.0F, 1.0F);
-        }
         List<String> ad = TeisyokuPlugin2.getInstance().TeisyokuConfig.getStringList("joinMessage");
         for (String s : ad) {
             player.sendMessage(Messages.getNormalPrefix() + color(s));

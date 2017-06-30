fad7e68925254f8ef73fe4c900b2968579529086
syokkendesuyo <syokkendesuyo@gmail.com>
2017-06-30 23:41:33 +0900

    カラーコードの表示に§0が無かった問題を修正

--- src/main/java/net/jp/minecraft/plugins/Commands/Command_Color.java
+++ src/main/java/net/jp/minecraft/plugins/Commands/Command_Color.java
@@ -13,7 +13,7 @@ import org.bukkit.command.CommandSender;
 public class Command_Color implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
         Msg.success(sender, "カラーコード一覧");
-        Msg.info(sender, "§11 §22 §33 §44 §55 §66 §77 §88 §99 §aa §bb §cc §dd §ee §ff");
+        Msg.info(sender, "§00 §11 §22 §33 §44 §55 §66 §77 §88 §99 §aa §bb §cc §dd §ee §ff");
         Msg.info(sender, "l§l:太字§r  m:§m打消し線§r  n:§n下線§r  o:§o斜体§r  r:リセット");
         return true;
     }

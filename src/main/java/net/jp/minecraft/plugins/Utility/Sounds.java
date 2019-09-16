843a2c981beb2a1c9d03255245c5238464aa47e8
syokkendesuyo <syokkendesuyo@gmail.com>
2019-09-16 09:32:55 +0900

    非推奨メソッドを削除

--- src/main/java/net/jp/minecraft/plugins/Utility/Sounds.java
+++ src/main/java/net/jp/minecraft/plugins/Utility/Sounds.java
@@ -12,26 +12,12 @@ import org.bukkit.event.Listener;
  */
 public class Sounds implements Listener {
 
-    @Deprecated
-    public static void sound_note(Player player) {
-        player.playSound(player.getLocation(), org.bukkit.Sound.BLOCK_NOTE_PLING, 3.0F, 1.5F);
-    }
-
-    @Deprecated
-    public static void sound_arrow(Player player) {
-        player.playSound(player.getLocation(), Sound.ENTITY_ARROW_SHOOT, 10.0F, 1.0F);
-    }
-
-    @Deprecated
-    public static void sound_villager(Player player) {
-        player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_DEATH, 10.0F, 1.0F);
-    }
-
-    @Deprecated
-    public static void sound_zombie_villager(Player player) {
-        player.playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_VILLAGER_CURE, 1.0F, 1.0F);
-    }
-
+    /**
+     * サウンドを再生
+     *
+     * @param player プレイヤー
+     * @param sound  サウンド
+     */
     public static void play(Player player, Sound sound) {
         player.playSound(player.getLocation(), sound, 10.0F, 1.0F);
     }

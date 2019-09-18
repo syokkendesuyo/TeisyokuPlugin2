37f5834fb1b6b8c2932a3fab5c8cb98d4666cb7b
syokkendesuyo <syokkendesuyo@gmail.com>
2019-09-19 02:14:29 +0900

    チャットに関するメソッドをリファクタリング

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_Chat.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_Chat.java
@@ -1,55 +1,40 @@
 package net.jp.minecraft.plugins.Listener;
 
 import net.jp.minecraft.plugins.API.API_Nick;
-import net.jp.minecraft.plugins.TeisyokuPlugin2;
+import net.jp.minecraft.plugins.API.API_PlayerDatabase;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * TeisyokuPlugin2
  *
- * @auther syokkendesuyo azuhata
+ * @author syokkendesuyo azuhata
  */
 public class Listener_Chat implements Listener {
 
-    private static File cfile;
-    private static FileConfiguration config;
-    private static File df = TeisyokuPlugin2.getInstance().getDataFolder();
-
     @EventHandler
     public void PlayerChat(AsyncPlayerChatEvent event) {
-
-        cfile = new File(df, "PlayerDatabase" + File.separator + event.getPlayer().getUniqueId() + ".yml");
-        config = YamlConfiguration.loadConfiguration(cfile);
-        FileConfiguration playerData = config;
 
         Player player = event.getPlayer();
         try {
             String NickName = API_Nick.getNick(player);
             String sender = event.getPlayer().getName();
 
-            String Name;
             if (NickName == null || NickName.equals("")) {
-                Name = sender;
-                player.setDisplayName(Name);
+                player.setDisplayName(sender);
             } else {
-                String color = playerData.getString("nick_color");
+                String color = API_PlayerDatabase.getString(player, "nick_color");
                 if (color.equalsIgnoreCase("default")) {
-                    Name = ChatColor.GRAY + "" + NickName + " " + ChatColor.RESET + sender;
-                    player.setDisplayName(Name);
+                    player.setDisplayName(ChatColor.GRAY + "" + NickName + " " + ChatColor.RESET + sender);
                     return;
                 }
-                Name = "§" + color + "" + NickName + " " + ChatColor.RESET + sender;
-                player.setDisplayName(Name);
+                player.setDisplayName("§" + color + "" + NickName + " " + ChatColor.RESET + sender);
             }
         } catch (Exception e) {
             e.printStackTrace();

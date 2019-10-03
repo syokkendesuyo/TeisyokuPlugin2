f5680a35841b2a83f8fa5f81d97f50326f0b9ba8
syokkendesuyo <syokkendesuyo@gmail.com>
2019-10-04 00:04:14 +0900

    Update Listener_SignColor.java

--- src/main/java/net/jp/minecraft/plugins/Listener/Listener_SignColor.java
+++ src/main/java/net/jp/minecraft/plugins/Listener/Listener_SignColor.java
@@ -5,20 +5,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 
+import java.util.Objects;
+
 /**
  * TeisyokuPlugin2
  *
- * @auther syokkendesuyo
+ * @author syokkendesuyo
  */
 public class Listener_SignColor implements Listener {
     @EventHandler
     public void onSignChangeEvent(SignChangeEvent event) {
-        if (!event.getPlayer().hasPermission("teisyoku.user")) {
-            return;
-        }
-        event.setLine(0, ChatColor.translateAlternateColorCodes('&', event.getLine(0)));
-        event.setLine(1, ChatColor.translateAlternateColorCodes('&', event.getLine(1)));
-        event.setLine(2, ChatColor.translateAlternateColorCodes('&', event.getLine(2)));
-        event.setLine(3, ChatColor.translateAlternateColorCodes('&', event.getLine(3)));
+        event.setLine(0, ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(event.getLine(0))));
+        event.setLine(1, ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(event.getLine(1))));
+        event.setLine(2, ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(event.getLine(2))));
+        event.setLine(3, ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(event.getLine(3))));
     }
 }

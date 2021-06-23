10aca6573bd9d177d39a242ad4dd24617284fc58
Yuku <yuku1f@gmail.com>
2021-06-23 09:40:50 +0900

    util.BlockUtil.isSign()を削除

--- src/main/java/net/jp/minecraft/plugins/teisyokuplugin2/util/BlockUtil.java
+++ src/main/java/net/jp/minecraft/plugins/teisyokuplugin2/util/BlockUtil.java
@@ -1,37 +1,9 @@
 package net.jp.minecraft.plugins.teisyokuplugin2.util;
 
 import org.bukkit.Material;
-
 import org.bukkit.block.data.Rail;
 
 public class BlockUtil {
-
-    /**
-     * 看板かどうかを判定するメソッド
-     *
-     * @param material 検査するmaterial
-     * @return 看板か否か(trueは看板)
-     */
-    public static boolean isSign(Material material) {
-
-        // 新しい看板が追加されたらここに追加する
-        Material[] targets = {
-                Material.ACACIA_SIGN,
-                Material.ACACIA_WALL_SIGN,
-                Material.BIRCH_SIGN,
-                Material.BIRCH_WALL_SIGN,
-                Material.DARK_OAK_SIGN,
-                Material.DARK_OAK_WALL_SIGN,
-                Material.JUNGLE_SIGN,
-                Material.JUNGLE_WALL_SIGN,
-                Material.OAK_SIGN,
-                Material.OAK_WALL_SIGN,
-                Material.SPRUCE_SIGN,
-                Material.SPRUCE_WALL_SIGN};
-
-        return scanMaterial(material, targets);
-    }
-
     /**
      * 指定のmaterialがmaterial配列のなかにあるかどうか検査するメソッド
      * @param material 検査するmaterial
